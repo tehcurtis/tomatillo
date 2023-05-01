fb80262c8bf1a09d57afb105b68375bae7abb55f
curtis <curtis.edmond@gmail.com>
2023-04-30 20:51:07 -0600

    ensure submitted questions end with a '?'

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,12 @@
 class Question < ActiveRecord::Base
 
+  before_save :ensure_question_mark
+
+  private
+
+  def ensure_question_mark
+    return if question.ends_with?("?")
+
+    self.question = question + "?"
+  end
 end
