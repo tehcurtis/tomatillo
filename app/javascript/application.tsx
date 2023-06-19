04415b5fc4d46e650b5a464d77e6b0fd04583009
curtis <curtis.edmond@gmail.com>
2023-06-19 08:50:14 -0600

    rename root component

--- app/javascript/application.tsx
+++ app/javascript/application.tsx
@@ -1,7 +1,6 @@
-import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import FormContainer from './FormContainer';
 
-const container = document.getElementById("FormContainer")!;
+const container = document.getElementById("CompContainer")!;
 const root = createRoot(container);
 root.render(<FormContainer />);
