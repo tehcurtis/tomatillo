24eaf6674ab76d02f831074c4b618dd9c639c71b
curtis <curtis.edmond@gmail.com>
2023-07-04 09:44:31 -0600

    wire up the 404 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+
+  def render_404
+    respond_to do |format|
+      format.html { render file: 'public/404.html', status: 404, layout: false }
+    end
+  end
 end
